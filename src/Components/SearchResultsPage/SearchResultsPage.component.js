cc7143eadf38f69144a5635f6d0a03b54e07f345
Brian Berg <bberg11@gmail.com>
2020-06-02 22:20:13 -0400

    Output current query in SearchResultsPage heading

--- src/Components/SearchResultsPage/SearchResultsPage.component.js
+++ src/Components/SearchResultsPage/SearchResultsPage.component.js
@@ -1,16 +1,33 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 import MovieList from 'Components/MovieList/MovieList.component';
 
 import './SearchResultsPage.styles.scss';
 
-const SearchResultsPage = () => {
+const SearchResultsPage = ({ match: { params } }) => {
   return (
     <section className="search-results-page">
-      <h1>Search Results Page</h1>
+      <h1>
+        Search Results For: <em>&quot;{params.query}&quot;</em>
+      </h1>
       <MovieList />
     </section>
   );
+};
+
+SearchResultsPage.defaultProps = {
+  match: {
+    params: {},
+  },
+};
+
+SearchResultsPage.propTypes = {
+  match: PropTypes.shape({
+    params: PropTypes.shape({
+      query: PropTypes.string,
+    }),
+  }),
 };
 
 export default SearchResultsPage;
