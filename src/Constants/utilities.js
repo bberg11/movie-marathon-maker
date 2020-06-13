443e04499c6a53511b6c24d2de4ad128c3dd6bb3
Brian Berg <bberg11@gmail.com>
2020-06-12 20:05:13 -0400

    Fix logic bug with negative display times. Math.floor always rounds down with negative values, causing an extra hour to be added to display value

--- src/Constants/utilities.js
+++ src/Constants/utilities.js
@@ -2,7 +2,8 @@
 import config from 'Constants/config';
 
 export const convertMinutesForDisplay = (time) => {
-  const hours = time / 60;
+  const positiveTime = time < 0 ? time * -1 : time;
+  const hours = positiveTime / 60;
   const roundedHours = Math.floor(hours);
   const minutes = (hours - roundedHours) * 60;
   const roundedMinutes = Math.round(minutes);
@@ -15,6 +16,10 @@ export const convertMinutesForDisplay = (time) => {
 
   if (display === '') {
     return '0 minutes';
+  }
+
+  if (time < 0) {
+    display = `-${display}`;
   }
 
   return display;
